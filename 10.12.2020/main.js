9a7c0903a30fd5dd8e7c2208da71e10d452fdc81
NourisGr <46955989+NourisGr@users.noreply.github.com>
2020-12-10 09:32:15 +0100

    changes

--- 10.12.2020/main.js
+++ 10.12.2020/main.js
@@ -1,13 +1,16 @@
-var countries = ["Luxembourg", "USA", "France", "Germany", "Italy"];
+var countries = ["Luxembourg", "Greece", "France", "Germany", "Italy"];
 
 function fillInSelect() {
     for (let i = 0; i < countries.length; i++) {
-        document.getElementById("listOfCountries").innerHTML += '<option>' + countries[i] + "</option>";
+        document.getElementById("listOfCountries").innerHTML += "<option value=" + i + ">" + countries[i] + "</option>";
     }
 }
 
 function SelectThisCountry() {
-    let a = document.getElementById("listOfCountries").value;
-    //alert(a);
-    document.getElementById("selectedCountries").innerHTML += '<li>' + a + '</li>';
+    let currentSelectCountry = document.getElementById("listOfCountries").value;
+    if (currentSelectCountry == -1) {
+        alert("Wrong choice");
+    } else {
+        document.getElementById("selectedCountries").innerHTML += '<li>' + countries[currentSelectCountry] + "</li>";
+    }
 }
