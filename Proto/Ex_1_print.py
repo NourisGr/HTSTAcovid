4207f0dc61d7d1568d6d60d3d410ba0389918ace
NourisGr <46955989+NourisGr@users.noreply.github.com>
2020-12-16 09:13:25 +0100

    general changes

---
+++ Proto/Ex_1_print.py
@@ -0,0 +1,5 @@
+Name = input("Please enter your name: ")
+Age = int(input("Please enter your age: "))
+
+print("Yourname is:", Name, "and your age is:", Age)
+print("Your age after 100 years is",Age + 100, "years")
