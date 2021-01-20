1192fbf05bf7379a809f717ba48bb405dc427154
LPEM Computer <nouris.lux@yahoo.com>
2021-01-20 09:38:50 +0100

    js

---
+++ Cliss/14.01.2021/main.js
@@ -0,0 +1,15 @@
+function start()
+    for(i=1;i<=20;i++)        
+        {
+            n = n + 1;
+            if(n == 20)
+            {
+                document.getElementById("cliss").innerHTML += n +'+';
+            } 
+            else
+            {
+                document.getElementById("cliss").innerHTML += n+1
+            }
+            
+        }
+    }
