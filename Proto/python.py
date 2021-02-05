b193d24b8c75361cc2340752efc15824cb408647
nouri <nouris.lux@yahoo.com>
2021-02-05 22:06:38 +0100

    finished

---
+++ Proto/python.py
@@ -0,0 +1,6 @@
+name= input("What is your name?: ")
+age= int(input("What is your age that you are getting in 2021?: "))
+
+age100= 100-age
+year= 2021+age100
+print("You are going to turn 100 in the year of",year)
