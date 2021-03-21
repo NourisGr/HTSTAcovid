49052c2af647a5c51ae0ac5048a0386dc27ddb92
NourisGr <nouris.lux@yahoo.com>
2021-03-21 17:16:50 +0100

    update

--- Proto/Built-in List Functions Homework/Task_list_2.py
+++ Proto/Built-in List Functions Homework/Task_list_2.py
@@ -5,12 +5,15 @@ carlist = [["VW", "Golf", "red", "25400"], ["Nissan", "Micra", "blue", "6400"],[
 print(carlist)
 
 #3) Print only the first element ["VW", "Golf", "red", "25400"] of the carlist over the terminal.
-print(carlist[0])
+ex3 = carlist[0]
+print(ex3)
 
 #4) Print only the color of the first element [ "red"] of the carlist over the terminal.
-
+color = carlist[0][2]
+print(color)
 #5) Replace the color of the first element [ "red"] of the carlist to [ "green"].
-
+carlist[0][2] = "green"
+print(carlist)
 
 
 
