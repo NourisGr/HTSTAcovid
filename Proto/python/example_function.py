6a573752cf2054f6871a44108b9e9d259de394e2
NourisGr <nouris.lux@yahoo.com>
2021-05-05 10:09:39 +0200

    Update

--- Proto/python/example_function.py
+++ Proto/python/example_function.py
@@ -1,4 +1,4 @@
-def total (value1 , value2): 
+def total (value1 , value2):  #initialization of the function
 
     totalValue = value1 + value2
 
@@ -12,4 +12,16 @@ def total (value1 , value2):
 
 
 total(8 , 10) 
-print( "Total value is :", total (8,10))
+print( "Total value is :", total (8,10))    #first call function --> evem 
+print( "Total value is :", total (2,5))     #second call function --> odd
+
+print("Total value is :" , total(2))
+print("Total value is :" , total())
+
+case_1 = total(8,10)
+print ("What is the value of case 1?", case_1)
+returnValueElement1 = case_1[0]
+returnValueElement2 = case_1[1]
+returnValueElement3 = case_1[2]
+
+print(returnValueElement1,returnValueElement2,returnValueElement3)
