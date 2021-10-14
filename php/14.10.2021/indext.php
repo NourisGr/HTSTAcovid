da56502d14e41003bbc6ad734299252568780bca
NourisGr <nouris.lux@yahoo.com>
2021-10-14 11:35:25 +0200

    php

---
+++ php/14.10.2021/indext.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $ARR = [4,5,6];
+    $X = 10;
+    $Y = 20;
+
+    array_push($Y,$ARR);
+
+    var_dump($ARR);
+    print r($ARR)
+    ?>
+</body>
+</html>
