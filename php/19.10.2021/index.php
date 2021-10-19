bc9581eeb19bc26cb59d899bbcf169e4c67a8822
NourisGr <nouris.lux@yahoo.com>
2021-10-19 09:53:07 +0200

    push

--- php/19.10.2021/index.php
+++ php/19.10.2021/index.php
@@ -5,15 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+<style>
+    table, th, td {
+  border:1px solid black;
+}
+</style>
 </head>
+
 <body>
 <?php
 
 $costofCars = array("Dacia"=>"10000","Mercedes"=>"50000","Volvo"=>"60000","BMW"=>"45000","Audi"=>"90000");
-foreach ($costofCars as $Car) {
-    print($Car.",");
-}
 
 ?>
+
+<table>
+<tr><<th>Car</th><th>Price</th></tr>
+<?php
+foreach($costofCars as $key=>$value)
+{
+?>
+<tr><td><?=$key?></tb><tb><?=$value?></td></tr>
+
+<?php
+}
+?>
+
+</table>
 </body>
 </html>
