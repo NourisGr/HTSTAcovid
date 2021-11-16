44cdf5dd220c46650d9d632bcce7d9ee02e9aff2
NourisGr <nouris.lux@yahoo.com>
2021-11-16 09:34:38 +0100

    push

---
+++ php/website_php/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' media='screen' href='include/css/main.css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="navbar">
+		<a href="index.html"><i class="fa fa-fw fa-home"></i> Home</a>
+		<a href="about.html"><i class="fa fa-fw fa-info"></i> About</a>
+		<div class="links">
+            <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a>
+            <div class="dropdown">
+                <a href="Email.html"><i class="fa fa-envelope"></i> Email</a>
+                <a href="Phone.html"><i class="fa fa-phone"></i> Phone</a>
+                <a href="Address.html"><i class="fa fa-map-marker"></i> Address</a>
+            </div>
+        </div>
+		<a class="active"><i class="fa fa-fw fa-shopping-basket"></i> Products</a>
+		<a href="productsGR.html"> <i class="fa fa-language"></i> <img src="include/images/FlagofGreece.png"class="imgsize" alt="GR"></a>
+		<a href="register.html"><i class="fa fa-sign-in"></i></a>
+	</div>
+</body>
+</html>
